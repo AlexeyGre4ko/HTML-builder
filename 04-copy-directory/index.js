4ea6bb826d769d71ff6a59de7bd5667cd4c15a62
AlexeyGre4ko <alexeygre4ko@gmail.com>
2022-11-07 21:51:51 +0300

    feat: implement create new folder and copy files according task 04-copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
+
+
+async function main() {
+    const dirSrc = path.join(__dirname, 'files');
+    const dirDst = path.join(__dirname, 'files-copy');
+    await syncFolder(dirSrc, dirDst);
+}
+
+
+async function syncFolder(src, dst) {
+    await fsPromises.mkdir(dst, { recursive: true });
+
+    const unwantedFiles = await fsPromises.readdir(dst);
+    for (const file of unwantedFiles) {
+        const pathSrc = path.join(src, file);
+        const isFileMissingWithinSrc = await fsPromises.access(pathSrc, fs.constants.F_OK).then(() => false).catch(() => true);
+        if (isFileMissingWithinSrc) {
+            console.log(`  Removing unwanted dst file: ${file}`);
+            await fsPromises.rm(path.join(dst, file));
+        }
+    }
+
+    const files = await fsPromises.readdir(src);
+    for (const file of files) {
+        const fileSrc = path.join(src, file);
+        const fileDst = path.join(dst, file);
+        console.log(`  Copying file: ${file}`);
+        await fsPromises.copyFile(fileSrc, fileDst);
+    }
+};
+
+
+main();
 
